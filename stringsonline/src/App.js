291c23f7d7477eefd99cbac9a76d278846405915
Josefine-Falk <josefine1999.falk@live.dk>
2021-08-31 12:01:01 +0200

    ændringer

--- stringsonline/src/App.js
+++ stringsonline/src/App.js
@@ -2,18 +2,16 @@ import { Router } from 'react-router';
 import './App.css';
 import { AuthProvider } from './compontens/login/AuthProvider';
 import { Navigation } from './compontens/Navigation/navigation';
-import { Frontpage } from './pages/Frontpage/FrontPage';
 import { Routes } from './Routes/Routes';
 
 function App() {
   return (
-    <Frontpage/>
-  //   <AuthProvider>
-  // <Router>
-  //   <Navigation/>
-  //   <Routes/>
-  // </Router>
-  // </AuthProvider>
+    <AuthProvider>
+  <Router>
+    <Navigation/>
+    <Routes/>
+  </Router>
+  </AuthProvider>
   );
 }
 
